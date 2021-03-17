9e527df4dce87558950220faf7f5ed17ca62f3f7
GinoChasles <gino.chasles@gmail.com>
2021-03-17 10:58:37 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,14 @@
+module.exports = ({env}) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3410ee0fc4c1d081cefe4952b1b72a1'),
+    },
+  },
+  cors: {
+    enabled: true,
+    headers: "*",
+    origin: ["https://blog-lsf4all.herokuapp.com/", "http://localhost"]
+  }
+});
